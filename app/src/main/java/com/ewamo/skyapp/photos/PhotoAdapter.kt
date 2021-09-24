e5634ee953c09d0ae9eaaceb5aa3cfe1baceea3e
ewamo <ewamo@DESKTOP-KH7STAU.local>
2021-09-24 22:31:27 +0200

    load photo with Picasso

--- app/src/main/java/com/ewamo/skyapp/photos/PhotoAdapter.kt
+++ app/src/main/java/com/ewamo/skyapp/photos/PhotoAdapter.kt
@@ -2,29 +2,29 @@ package com.ewamo.skyapp.photos
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.AdapterView
 import androidx.recyclerview.widget.RecyclerView
 import com.ewamo.skyapp.data.Photo
 import com.ewamo.skyapp.databinding.ItemPhotoBinding
+import com.squareup.picasso.Picasso
 
 class PhotoAdapter(
     private val photos: ArrayList<Photo>
 ) :
     RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>() {
 
-//    private lateinit var binding: ItemPhotoBinding
+    //    private lateinit var binding: ItemPhotoBinding
     var onItemClick: (Int) -> Unit = {}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
         val binding = ItemPhotoBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
 
-//        val itemView = binding.root
-//
-//        val itemHolder = PhotoViewHolder(itemView)
-//        itemView.setOnClickListener {
-//            onItemClick(itemHolder.adapterPosition)
-//        }
+        val itemView = binding.root
+
+        val itemHolder = PhotoViewHolder(binding)
+        itemView.setOnClickListener {
+            onItemClick(itemHolder.adapterPosition)
+        }
 
         return PhotoViewHolder(binding)
     }
@@ -39,20 +39,13 @@ class PhotoAdapter(
     inner class PhotoViewHolder(private val binding: ItemPhotoBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        init {
-            binding.apply {
-                val position = adapterPosition
-//                if (position != RecyclerView.NO_POSITION) {
-//                    val photo = getItem(position)
-//                    listener.onItemClick(photo)
-//                }
-            }
-            //todo
-        }
+
+
 
         fun bind(photo: Photo) {
             binding.apply {
-                //todo
+
+                Picasso.get().load(photo?.url).into(itemImage)
             }
         }
 
