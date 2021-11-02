0e0801192027afdcd97295004fe29c8a489c8af3
ewamo <ewamo@DESKTOP-KH7STAU.local>
2021-11-02 21:21:42 +0100

    add data binding

--- app/src/main/java/com/ewamo/skyapp/data/BindingAdapter.kt
+++ app/src/main/java/com/ewamo/skyapp/data/BindingAdapter.kt
@@ -1,4 +1,10 @@
 package com.ewamo.skyapp.data
 
-class BindingAdapter {
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.squareup.picasso.Picasso
+
+@BindingAdapter("imgUrl")
+fun bindMyImage(imgView: ImageView, imgUrl: String?){
+    Picasso.get().load(imgUrl).into(imgView)
 }
